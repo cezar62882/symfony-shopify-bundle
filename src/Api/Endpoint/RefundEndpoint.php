ddfd03da6f53470f7a6179dc83e8cf4cb3dcdeb5
vitaly.gorbunov <cezar62882@gmail.com>
2018-11-21 11:11:06 +0300

    create refund

--- src/Api/Endpoint/RefundEndpoint.php
+++ src/Api/Endpoint/RefundEndpoint.php
@@ -1,7 +1,9 @@
 <?php
 namespace CodeCloud\Bundle\ShopifyBundle\Api\Endpoint;
 
+use CodeCloud\Bundle\ShopifyBundle\Api\GenericResource;
 use CodeCloud\Bundle\ShopifyBundle\Api\Request\GetJson;
+use CodeCloud\Bundle\ShopifyBundle\Api\Request\PostJson;
 
 class RefundEndpoint extends AbstractEndpoint
 {
@@ -15,6 +17,18 @@ class RefundEndpoint extends AbstractEndpoint
     {
         $params = $fields ? array('fields' => implode(',', $fields)) : array();
         $request = new GetJson('/admin/orders/' . $orderId . '/refunds/' . $refundId . '.json', $params);
+        $response = $this->send($request);
+        return $this->createEntity($response->get('refund'));
+    }
+
+    /**
+     * @param int $orderId
+     * @param array $refund
+     * @return GenericResource
+     */
+    public function create($orderId, GenericResource $refund)
+    {
+        $request = new PostJson("/admin/orders/$orderId/refunds.json", array('refund' => $refund->toArray()));
         $response = $this->send($request);
         return $this->createEntity($response->get('refund'));
     }
